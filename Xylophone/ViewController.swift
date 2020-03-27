95013076b4130312d7bf484490ee5b11edada16c
stefan <stefanchelaru96@gmail.com>
2020-03-27 13:54:45 +0200

    finished

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -25,6 +25,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
         playSound(soundName: sender.currentTitle!)
     }
     
